271543614f09d2eac007d2b4f36b7ff5f51e193f
agustin rosano <arosano@goldmund.com>
2024-03-18 14:10:25 -0300

    cambios en el navabar de lado

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -9,12 +9,13 @@ export const NavBar = () => {
     <div className="container-fluid d-flex justify-content-evently " style={{fontSize:'25px'}}>
       <h1 style={{fontSize:'30px', marginRight:'30px'}} > Thomas Amado </h1>
       
+      
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarNav" >
-        <div className='d-flex justify-content-end'>
-            <ul className="navbar-nav " style={{marginLeft:'10px'}}>
+      <div className="collapse navbar-collapse " id="navbarNav" >
+        <div className='d-flex justify-content-end '>
+            <ul className="navbar-nav "  style={{marginLeft:'10px'}}>
               <li>  
                 <Link to='/ 'className="nav-item nav-link" style={{fontSize:"22px"}}>
                       Home
